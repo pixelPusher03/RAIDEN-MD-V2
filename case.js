335018aa624c963038bcea0c3273ad9a396c7481
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2024-12-31 21:42:50 +0300

    Update case.js

--- case.js
+++ case.js
@@ -1786,6 +1786,83 @@ CPU: ${server.limits.cpu}%
 }
 
 break
+case 'chatgpt': {
+    if (!text) {
+        reply('Please ask me something!');
+        return;
+    }
+
+    try {
+        const apiUrl = `https://api.davidcyriltech.my.id/ai/chatbot?query=${encodeURIComponent(text)}`;
+
+
+        const response = await fetch(apiUrl);
+        const jsonData = await response.json();
+
+        
+        if (jsonData.success && jsonData.result) {
+            reply(jsonData.result); 
+        } else {
+            reply('Failed to fetch response from the API. Please try again later.');
+        }
+    } catch (error) {
+        console.error('Error fetching API response:', error);
+        reply('An error occurred while fetching the AI response. Please try again later.');
+    }
+    break
+case 'play': {
+    if (!text) return reply(`*Example*: ${prefix + command} Faded by Alan Walker`);
+
+    try {
+       
+        await David.sendMessage(m.chat, { react: { text: `🎵`, key: m.key } });
+
+        
+        const yts = require("yt-search");
+        const search = await yts(text);
+        const video = search.videos[0]; 
+
+        if (!video) {
+            reply(`*No results found for:* ${text}`);
+            return;
+        }
+
+        
+        const body = `*RAIDEN MD V2_MUSIC - PLAYER*\n` +
+                     `> *Title:* ${video.title}\n` +
+                     `> *Views:* ${video.views}\n` +
+                     `> *Duration:* ${video.timestamp}\n` +
+                     `> *Uploaded:* ${video.ago}\n` +
+                     `> *Url:* ${video.url}\n` +
+                     `> ᴘᴏᴡᴇʀᴇᴅ ʙʏ Rᴀɪᴅᴇɴ MD V2`;
+
+        await David.sendMessage(m.chat, {
+            image: { url: video.thumbnail },
+            caption: body
+        }, { quoted: m });
+
+        
+        const apiUrl = `https://api.davidcyriltech.my.id/download/ytmp3?url=${encodeURIComponent(video.url)}`;
+        const apiResponse = await axios.get(apiUrl);
+
+        if (apiResponse.data.success) {
+            const { download_url, title, thumbnail } = apiResponse.data.result;
+
+      
+            await David.sendMessage(m.chat, {
+                audio: { url: download_url },
+                mimetype: 'audio/mp4',
+                fileName: `${title}.mp3`,
+                caption: `🎧 *Here's your song:*\n> *Title:* ${title}`
+            }, { quoted: m });
+        } else {
+            reply(`*Failed to fetch the song! Please try again later.*`);
+        }
+    } catch (error) {
+        console.error('Error during /play command:', error);
+        reply(`*An error occurred while processing your request. Please try again later.*`);
+    }
+    break
 case 'cekasalmember': {
   if (!m.isGroup) return m.reply(mess.group)
   const participants = await farisofc.groupMetadata(m.chat).then(metadata => metadata.participants);
